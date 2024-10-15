b68983845bd46b3cb411113e0aa3faefc80ce56d
1ngii <helgib.2005@gmail.com>
2024-10-15 06:46:55 -0700

    Update and rename home.php to products.php

--- products.php
+++ products.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Home
+Plugin Name: Products API
 Author: Helgi Bjarnason
 Description: Just a test for me to get used to shortcodes, plugins and using API
  */
@@ -8,8 +8,39 @@ Description: Just a test for me to get used to shortcodes, plugins and using API
 function get_products_data($atts){
     $response = wp_remote_get('https://dummyjson.com/products');
     $data = json_decode($response["body"]);
+    $filter = isset($_GET['filter']) ? intval($_GET['filter']) : 0;
+    $search_query = isset($_GET['search']) ? sanitize_text_field($_GET['search']) : '';
+    $output = "
+    <form method=\"get\">
+    <input type=\"search\" name=\"search\" placeholder=\"Search products\" value=\"" . esc_attr(isset($_GET['search']) ? $_GET['search'] : '') . "\">
+
+     <label for=\"filter\">Filter shown data : </label>
+        <select name=\"filter\" id=\"cmbMake\">
+        ";
+
+        $options = [
+            0 => "Any",
+            1 => "Price over $500",
+            2 => "Price under $500",
+            3 => "Multiple images"
+        ];
+        
+        foreach ($options as $value => $label){
+            $selected = $filter == $value ? 'selected' : '';
+            $output.= "<option value= \"$value\" $selected>$label</option>";
+        }
+        $output .= "
+            </select>
+            <button type=\"submit\" >Search</button>
+    </form>
+    ";
+
     
-    $output = '
+
+    
+    
+    
+    $output .= '
     <style>
         .products-table {
             border-collapse: collapse;
@@ -40,39 +71,36 @@ function get_products_data($atts){
     <th>Images</th>
     </tr></thead>
     <tbody>";
+
     foreach($data->products as $product){
+        if ($search_query && stripos($product->title, $search_query) === false) {
+            continue;
+        }
+        
         $images = '';
         foreach($product->images as $image)
         {
             $images .= "<img src='" . $image . "' alt='" . $product->title . "'/>";
         }
+
+        if ($filter == 0 ||
+            // ($filter == 1 && !empty($product->title))||
+            ($filter == 1 && $product->price > 500) ||
+            ($filter == 2 && $product->price < 500) ||
+            ($filter == 3 && count($product->images) > 1)){
+
             $output .= "<tr>
             <td> $product->title </td>
             <td>" . number_format($product->price, 2) . "</td>
             <td> $images </td>
             </tr>";
+        }
     }
     $output .= "</tbody></table>";
 
+    
     return $output;
 }
-function filter_api_data($content){
-    $output = "
-    <label for=\"filter\">Filter shown data : </label>
-    <select name=\"Make\" id=\"cmbMake\">
-        <option value=\"any\">Any</option>
-        <option value=\"products\">Products</option>
-        <option value=\"price\">Price</option>
-        <option value=\"images\">Images</option>
-    </select>
-    <button>Submit</button>";
 
-    return $output;
-}
-// function search_data(){
-
-// }
-add_shortcode('products_api', 'get_products_data');
-add_shortcode( 'api_filter', 'filter_api_data' );
-// add_filter( 'search', 'search_data');
+add_shortcode( 'products_api', 'get_products_data' );
 ?>
