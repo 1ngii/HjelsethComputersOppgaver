b9aece1494b4580df14560f9518735639ceeb13a
1ngii <helgib.2005@gmail.com>
2024-10-17 01:22:00 -0700

    Update wc-autofill.php

--- wc-autofill.php
+++ wc-autofill.php
@@ -86,60 +86,6 @@ function wcap_fetch_and_create_products_from_json($file) {
     }
 }
 
-// Function to fetch and create products
-// function wcap_fetch_and_create_products() {
-//     $response = wp_remote_get('https://dummyjson.com/products'); 
-
-//     if (is_wp_error($response)) {
-//         echo '<div class="error"><p>Error fetching products.</p></div>';
-//         return;
-//     }
-
-//     $data = json_decode($response['body']);
-//     $product_count = 0;
-
-//     if (!empty($data->products)) {
-//         foreach ($data->products as $product_data) {
-//             // Check if the product already exists
-//             $existing_product_id = wc_get_product_id_by_sku($product_data->id);
-
-//             if (!$existing_product_id) {
-//                 // Create a new product
-//                 $product = new WC_Product_Simple();
-//                 $product->set_name($product_data->title);
-//                 $product->set_regular_price($product_data->price);
-//                 $product->set_description($product_data->description);
-//                 $product->set_sku($product_data->id);
-//                 $product->set_stock_status('instock');
-
-//                 // Handle images
-//                 if (!empty($product_data->images)) {
-//                     $image_ids = [];
-//                     foreach ($product_data->images as $image_url) {
-//                         $image_id = wcap_upload_image($image_url);
-//                         if ($image_id) {
-//                             $image_ids[] = $image_id;
-//                         }
-//                     }
-//                     $product->set_image_id($image_ids[0]); // Set the first image as the product image
-//                     $product->set_gallery_image_ids($image_ids); // Set the rest as gallery images
-//                 }
-
-//                 $product->save();
-//                 $product_count ++;
-//             }
-//         }
-//         if ($product_count > 0) {
-//             echo '<div class="updated"><p>'. $product_count .' Products have been autofilled successfully!</p></div>';
-//         }
-//         elseif ($product_count == 0) {
-//             echo '<div class="updated"><p>No product was autofilled, as they already exist.</p></div>';
-//         }
-//     } else {
-//         echo '<div class="error"><p>No products found in API response.</p></div>';
-//     }
-// }
-
 // Helper function to upload images to the media library
 function wcap_upload_image($image_url) {
     $response = wp_remote_get($image_url);
