ec2ea4a1b2c24e3bafac45b82d2bb3b02a63dd6e
1ngii <helgib.2005@gmail.com>
2024-10-18 01:24:01 -0700

    Update w/gpt newsub.php

--- newsub.php
+++ newsub.php
@@ -8,8 +8,9 @@ Description: Adds subscription as metadata in all product types. A reboot of the
 
 add_action( 'woocommerce_product_options_general_product_data', 'ingii_option_group' );
 
-function ingii_option_group(){
-    echo'<div class="options_group">';
+function ingii_option_group() {
+    // Render the checkbox
+    echo '<div class="options_group">';
     woocommerce_wp_checkbox(
         array(
             'id'    => 'sub',
@@ -18,42 +19,52 @@ function ingii_option_group(){
             'desc_tip' => true,
             'description' => 'This is a subscription type product with recurring payments'
         )
-        );
-    echo'</div>';
+    );
+
+    // Render the select box for subscription options
+    $subscription_value = get_post_meta( get_the_ID(), 'subscription_options', true );
+    echo '<div class="subscription_options" style="display: ' . (get_post_meta( get_the_ID(), 'subscription_product', true ) === 'yes' ? 'block' : 'none') . ';">';
+    woocommerce_wp_select([
+        'id' => 'subscription_options',
+        'label' => __( 'Select a subscription option', '' ),
+        'options' => [
+            '1' => '1 Month',
+            '2' => '3 Months',
+            '3' => 'Year'
+        ],
+        'desc' => __('Choose one option from the dropdown'),
+        'value' => $subscription_value,
+    ]);
+    echo '</div>';
+
+    echo '</div>'; // Close options_group
+
+    // Enqueue JavaScript to show/hide the select box based on checkbox
+    wc_enqueue_js("
+        jQuery('#sub').change(function(){
+            if(jQuery(this).is(':checked')) {
+                jQuery('.subscription_options').show();
+            } else {
+                jQuery('.subscription_options').hide();
+            }
+        }).change(); // Trigger change to set initial state
+    ");
 }
 
-// function subscription_pricing_option_group(){
-//     echo'<div class="">';
-//     woocommerce_wp_text_input([
-//         'id'  => 'sub_pricing',
-//         ''  => '',
-//         ''  => '',
-//         ''  => '',
-//         ''  => '',
-//     ]);
-//     echo'</div>';
-// }
+add_action( 'woocommerce_process_product_meta', 'ingii_save_field' );
 
-add_action( 'woocommerce_process_product_meta','ingii_save_field');
-
-function ingii_save_field( $id ){
-    $subscription = isset( $_POST[ 'sub' ] ) && 'yes' === $_POST[ 'sub' ] ? 'yes' : 'no';
+function ingii_save_field( $id ) {
+    // Save subscription checkbox value
+    $subscription = isset( $_POST['sub'] ) && 'yes' === $_POST['sub'] ? 'yes' : 'no';
     update_post_meta( $id, 'subscription_product', $subscription );
 
-    if ($subscription == 'yes'){
-        $sub_options = [
-        '1' => '1 Month',
-        '2' => '3 Months',
-        '3' => 'Year'
-    ];
-
-    woocommerce_wp_select([
-        'id'  =>  'subscription_options',
-        'label'  =>  __( 'Select a subscription option', '' ),
-        'options'  =>  $sub_options,
-        'desc'  =>  __('Choose one option from the dropdown'),
-    ]);
+    // Save subscription option selected
+    if ($subscription == 'yes') {
+        $subscription_option = isset($_POST['subscription_options']) ? $_POST['subscription_options'] : '';
+        update_post_meta($id, 'subscription_options', $subscription_option);
+    } else {
+        // Optionally delete the subscription option if it's not a subscription
+        delete_post_meta($id, 'subscription_options');
     }
 }
-
 ?>
